ae7e7cd0b07f84551f00b2719b03cd715a6d79ed
vueadmin <keikeizhang540623@gmail.com>
2022-06-25 16:21:08 +0800

    重新优化一下代码

--- index.js
+++ index.js
@@ -4,35 +4,48 @@ import swap from "./chn/swap.json";
 
 // 等待用装饰器进行优化
 class Festival {
-  constructor() { }
-  valid(date) {
-    return date > 20221230 || date < 20070101 ? true : false;
+  invalid(date) {
+    return date > 20221230 || date < 20070101;
   }
   day(date) {
-    return this.valid(date) ? "error message: invalid parameter" :
-      {
-        date,
-        name: holiday[date] === undefined ? "非假期" : holiday[date],
-        isHoliday: holiday[date] === undefined ? false : true,
-        isLegal: legal.includes(date),
-        isSwap: swap.includes(date),
-      }
+    if (this.invalid(date)) {
+      throw new Error("invalid parameter")
+    }
+    return {
+      date,
+      name: holiday[date] === undefined ? "非假期" : holiday[date],
+      isHoliday: holiday[date] === undefined ? false : true,
+      isLegal: legal.includes(date),
+      isSwap: swap.includes(date),
+    }
   }
   // 假期名称
   name(date) {
-    return this.valid(date) ? "error message: invalid parameter" : holiday[date] === undefined ? "非假期" : holiday[date];
+    if (this.invalid(date)) {
+      throw new Error("invalid parameter")
+    }
+    return holiday[date] === undefined ? "非假期" : holiday[date];
   }
   // 是否为假期节假日
-  isHoliday = (date) => {
-    return this.valid(date) ? "error message: invalid parameter" : holiday[date] === undefined ? false : true;
+  isHoliday(date) {
+    if (this.invalid(date)) {
+      throw new Error("invalid parameter")
+    }
+    return holiday[date] === undefined ? false : true;
   }
   // 是否为法定节假日
   isLegal(date) {
-    return this.valid(date) ? "error message: invalid parameter" : legal.includes(date);
+    if (this.invalid(date)) {
+      throw new Error("invalid parameter")
+    }
+    return legal.includes(date);
   }
   // 是否为调休日
   isUnhappy(date) {
-    return this.valid(date) ? "error message: invalid parameter" : swap.includes(date);
+    if (this.invalid(date)) {
+      throw new Error("invalid parameter")
+    }
+    return swap.includes(date);
   }
 }
 export default Festival;
