162a13403b50ff88afb5a8b5bcde3836c646c48f
zhangchuang <keikeizhang540623@gmail.com>
2023-03-22 11:31:55 +0800

    优化一下代码

--- index.js
+++ index.js
@@ -1,59 +1,62 @@
-import holiday from "./chn/holiday.json";
-import legal from "./chn/legal.json";
-import swap from "./chn/swap.json";
+import holiday from './chn/holiday.json'
+import legal from './chn/legal.json'
+import swap from './chn/swap.json'
 
-// 等待用装饰器进行优化
 class Festival {
+  // 构造函数中初始化一些公共变量
   constructor() {
-    const _this = this;
-    return new Proxy(_this, {
-      get(target, prop) {
-        let value = target[prop];
-        if (typeof value === "function") {
-          value = value.bind(_this);
-          if (value !== "invalid") {
-            return (date, ...args) => {
-              if (_this.invalid(date)) {
-                throw new Error("invalid parameter") 
-              }
-              return value(date, ...args);
-            }
-          } else {
-            return value;
-          }
-        } else {
-          return value;
-        }
-      }
-    })
+    // 国家制定的最早和最晚的一个假期日期
+    this.START_DATE = 20070101
+    this.END_DATE = 20231230
+    // 缓存 legal 和 swap 数组
+    this.legalSet = new Set(legal)
+    this.swapSet = new Set(swap)
   }
-  invalid(date) {
-    return date > 20231230 || date < 20070101;
+
+  // 判断日期是否符合要求（限制在指定日期范围内）
+  isValidDate(date) {
+    return date >= this.START_DATE && date <= this.END_DATE
   }
-  day(date) {
-    return {
-      date,
-      name: holiday[date] === undefined ? "非假期" : holiday[date],
-      isHoliday: holiday[date] === undefined ? false : true,
-      isLegal: legal.includes(date),
-      isSwap: swap.includes(date),
-    }
+
+  // 获取假期名称
+  getHolidayName(date) {
+    return holiday[date] || '非假期'
   }
-  // 假期名称
-  name(date) {
-    return holiday[date] === undefined ? "非假期" : holiday[date];
+
+  // 获取假期集合
+  getHolidaySet() {
+    return new Set(Object.keys(holiday))
   }
-  // 是否为假期节假日
-  isHoliday(date) {
-    return holiday[date] === undefined ? false : true;
-  }
+
   // 是否为法定节假日
-  isLegal(date) {
-    return legal.includes(date);
+  isLegalHoliday(date) {
+    return this.legalSet.has(date)
   }
+
   // 是否为调休日
-  isUnhappy(date) {
-    return swap.includes(date);
+  isSwapHoliday(date) {
+    return this.swapSet.has(date)
   }
+
+  // 获取日期信息
+  getDayInfo(date) {
+    if (!this.isValidDate(date)) {
+      return 'invalid'
+    }
+    const holidaySet = this.getHolidaySet()
+    const isHoliday = holidaySet.has(date)
+    const name = this.getHolidayName(date)
+    const isLegal = this.isLegalHoliday(date)
+    const isSwap = this.isSwapHoliday(date)
+    return { date, name, isHoliday, isLegal, isSwap }
+  }
+
+  // 对外暴露的方法
+  day = (date) => this.getDayInfo(date)
+  name = (date) => this.getHolidayName(date)
+  isHoliday = (date) => this.getDayInfo(date).isHoliday
+  isLegal = (date) => this.isLegalHoliday(date)
+  isSwap = (date) => this.isSwapHoliday(date)
 }
-export default Festival;
+
+export default new Festival()
