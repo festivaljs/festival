b74942c445702178a69d9c65d77c50293ece14f1
zhangchuang <keikeizhang540623@gmail.com>
2023-03-22 12:23:58 +0800

    解决一下数据返回错误的问题

--- dist/festival.js
+++ dist/festival.js
@@ -794,93 +794,85 @@
   	20231008
   ];
 
-  const holidayMap = new Map(Object.entries(holiday));
-  const legalMap = new Map(legal.map((date) => [date, true]));
-  const swapMap = new Map(swap.map((date) => [date, true]));
+  const holidayMap = new Map(Object.entries(holiday).map(([dateStr, name]) => [Number(dateStr), name]));
+  const legalSet = new Set(legal.map((date) => Number(date)));
+  const swapSet = new Set(swap.map((date) => Number(date)));
 
   class Festival {
-    // 构造函数中初始化一些公共变量
     constructor() {
-      // 保存日期范围的代理
       this.proxy = new Proxy(
         { startDate: 20070101, endDate: 20231230 },
         {
           get: function (target, prop) {
-            return target[prop];
+            return target[prop]
           },
           set: function (target, prop, value) {
             if (!Number.isNaN(Number(value))) {
               target[prop] = Number(value);
-              return true;
+              return true
             }
-            return false;
-          },
+            return false
+          }
         }
       );
       this.cache = new Map();
     }
 
-    // 判断日期是否符合要求（限制在指定日期范围内）
     isValidDate(date) {
-      return date >= this.proxy.startDate && date <= this.proxy.endDate;
+      return date >= this.proxy.startDate && date <= this.proxy.endDate
     }
 
-    // 获取日期信息
     getDayInfo(date) {
       if (!this.isValidDate(date)) {
-        return 'invalid';
+        return 'invalid'
       }
       if (this.cache.has(date)) {
-        return this.cache.get(date);
+        return this.cache.get(date)
       }
       const holiday = holidayMap.get(date) || '非假期';
-      const isLegal = legalMap.has(date);
-      const isSwap = swapMap.has(date);
+      const isLegal = legalSet.has(date);
+      const isSwap = swapSet.has(date);
       const isHoliday = holiday !== '非假期';
       const result = { date, name: holiday, isHoliday, isLegal, isSwap };
       this.cache.set(date, result);
-      return result;
+      return result
     }
 
-    // 获取假期名称
     getHolidayName(date) {
-      return holidayMap.get(date) || '非假期';
+      return holidayMap.get(date) || '非假期'
     }
 
-    // 获取假期集合
     getHolidaySet() {
-      return new Set(Object.keys(holiday));
+      return new Set(Object.keys(holiday).map((dateStr) => Number(dateStr)))
     }
 
-    // 是否为法定节假日
     isLegalHoliday(date) {
-      return legalMap.has(date);
+      return legalSet.has(date)
     }
 
-    // 是否为调休日
     isSwapHoliday(date) {
-      return swapMap.has(date);
+      return swapSet.has(date)
     }
 
     // 对外暴露的方法
     day(date) {
-      return this.getDayInfo(date);
+      return this.getDayInfo(Number(date))
     }
 
     name(date) {
-      return this.getHolidayName(date);
+      return this.getHolidayName(Number(date))
     }
 
     isHoliday(date) {
-      return this.getDayInfo(date).isHoliday;
+      return this.getDayInfo(Number(date)).isHoliday
     }
 
     isLegal(date) {
-      return this.isLegalHoliday(date);
+      return this.isLegalHoliday(Number(date))
     }
 
     isSwap(date) {
-      return this.isSwapHoliday(date);
+      return this.isSwapHoliday(Number(date))
     }
   }
 
